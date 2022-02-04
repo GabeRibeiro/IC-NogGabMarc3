c2e0c30b991d16dc69752e1bf9ce8b1944662e5b
vm <msramos99@ua.pt>
2022-02-04 13:19:23 +0000

    bash2

--- Main/lang.sh
+++ Main/lang.sh
@@ -24,3 +24,29 @@ echo ""
 ./lang LanguageTexts/czech.txt LanguageTexts/maias.txt 4 0.9
 ./lang LanguageTexts/czech.txt LanguageTexts/maias.txt 5 0.9
 ./lang LanguageTexts/czech.txt LanguageTexts/maias.txt 6 0.9
+echo ""
+echo "Modelo danish1.txt, texto maias.txt"
+./lang LanguageTexts/danish1.txt LanguageTexts/maias.txt 2 0.1
+./lang LanguageTexts/danish1.txt LanguageTexts/maias.txt 3 0.1
+./lang LanguageTexts/danish1.txt LanguageTexts/maias.txt 4 0.1
+./lang LanguageTexts/danish1.txt LanguageTexts/maias.txt 5 0.1
+./lang LanguageTexts/danish1.txt LanguageTexts/maias.txt 6 0.1
+echo ""
+./lang LanguageTexts/danish1.txt LanguageTexts/maias.txt 2 0.3
+./lang LanguageTexts/danish1.txt LanguageTexts/maias.txt 3 0.3
+./lang LanguageTexts/danish1.txt LanguageTexts/maias.txt 4 0.3
+./lang LanguageTexts/danish1.txt LanguageTexts/maias.txt 5 0.3
+./lang LanguageTexts/danish1.txt LanguageTexts/maias.txt 6 0.3
+echo ""
+./lang LanguageTexts/danish1.txt LanguageTexts/maias.txt 2 0.6
+./lang LanguageTexts/danish1.txt LanguageTexts/maias.txt 3 0.6
+./lang LanguageTexts/danish1.txt LanguageTexts/maias.txt 4 0.6
+./lang LanguageTexts/danish1.txt LanguageTexts/maias.txt 5 0.6
+./lang LanguageTexts/danish1.txt LanguageTexts/maias.txt 6 0.6
+echo ""
+./lang LanguageTexts/danish1.txt LanguageTexts/maias.txt 2 0.9
+./lang LanguageTexts/danish1.txt LanguageTexts/maias.txt 3 0.9
+./lang LanguageTexts/danish1.txt LanguageTexts/maias.txt 4 0.9
+./lang LanguageTexts/danish1.txt LanguageTexts/maias.txt 5 0.9
+./lang LanguageTexts/danish1.txt LanguageTexts/maias.txt 6 0.9
+echo ""
